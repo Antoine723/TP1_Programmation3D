82a53bf7b673f9d2ea71faa9e4cb018626ca3aa3
Antoine <antoinea723@gmail.com>
2021-01-20 19:55:11 +0100

    Ajout modif centre en y

--- Assets/Scripts/CubePrefab.cs
+++ Assets/Scripts/CubePrefab.cs
@@ -8,8 +8,10 @@ using Object = UnityEngine.Object;
 public class CubePrefab : MonoBehaviour
 {
     [SerializeField] private int clonesChosen = 0; //variable qui contient le nombre de clones que l'utilisateur souhaite instancier
-    private float x=0;
-    private float z=0;
+    private float posx=0;
+    private float posy = 0;
+    private float posz=0;
+    
     [SerializeField] private float rayonChosen = 5f; //pareil que pour clonesChosen mais avec le rayon
     private float rayon = 5f;
     private float angle = 0;
@@ -18,28 +20,33 @@ public class CubePrefab : MonoBehaviour
     private GameObject cube;
     private GameObject lastCube; //dernier cube de la liste
     private GameObject centerCube;
-    private float xCenter; //position en x du cube du centre
-    private float zCenter; //position en z du cube du centre
-
+    private float posxCenter; //position en x du cube du centre
+    private float posyCenter;
+    private float poszCenter; //position en z du cube du centre
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         cubesList.Add(Resources.Load("Cube")); //On ajoute notre cube du centre dans la liste
         centerCube = GameObject.Find("Cube");
-        xCenter = centerCube.transform.position.x; //on stocke ses positions en x et en z dans les variables correspondantes
-        zCenter = centerCube.transform.position.z;
+        posxCenter = centerCube.transform.position.x; //on stocke ses positions en x en y et en z dans les variables correspondantes
+        posyCenter = centerCube.transform.position.y;
+        poszCenter = centerCube.transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if ( (clonesCreated != clonesChosen) || (rayon!=rayonChosen) || (centerCube.transform.position.x != xCenter) || (centerCube.transform.position.z != zCenter))
-            //Si l'utilisateur modifie le nombre de clones, le rayon ou la position en x ou en z du centre, on détruit tous les cubes
+        if ( (clonesCreated != clonesChosen) || (rayon!=rayonChosen) || (centerCube.transform.position.x != posxCenter) || (centerCube.transform.position.y != posyCenter) || (centerCube.transform.position.z != poszCenter))
+            //Si l'utilisateur modifie le nombre de clones, le rayon ou la position en x en y ou en z du centre, on détruit tous les cubes
             //en ré-assignant le nouveau centre du cercle
         {
-            xCenter = centerCube.transform.position.x;
-            zCenter = centerCube.transform.position.z;
+            posxCenter = centerCube.transform.position.x;
+            posyCenter = centerCube.transform.position.y;
+            poszCenter = centerCube.transform.position.z;
             for (int i = cubesList.Count-1; i > 0; i--)
             {
                 Destroy((Object) cubesList[i]);
@@ -53,11 +60,12 @@ public class CubePrefab : MonoBehaviour
         {
             lastCube = (GameObject)cubesList[cubesList.Count-1]; //on récupère le dernier cube de la liste à chaque fois pour pouvoir adapter
                                                                  //la position du nouveau cube en fonction de ce dernier
-            x = (float) (rayon * Math.Cos(angle)) + xCenter;
-            z = (float) (rayon * Math.Sin(angle)) +zCenter;
+            posx = (float) (rayon * Math.Cos(angle)) + posxCenter;
+            posy = posyCenter;
+            posz = (float) (rayon * Math.Sin(angle)) +poszCenter;
             angle += (float) (2 * Math.PI / clonesChosen); //On a un angle différent selon le cube (on incrémente donc "angle")
             cube = (GameObject) Instantiate(Resources.Load("Cube"), //on instancie notre préfab
-                new Vector3(x, transform.position.y, z),
+                new Vector3(posx, posy, posz),
                 Quaternion.identity);
             cubesList.Add(cube);
             clonesCreated += 1;
